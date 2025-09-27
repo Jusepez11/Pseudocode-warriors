10e36f36b67eee5f50d106980b174e8aeeac18c4
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2025-09-26 21:42:39 -0400

    Create index.py
    with this we can just load all the routers into the file that will load the fastAPI

---
+++ src/api/routers/index.py
@@ -0,0 +1,13 @@
+from . import (
+    user,
+    ingredient,
+    pantry_ingredient,
+    recipe,
+)
+
+
+def load_routes(app):
+    app.include_router(user.router)
+    app.include_router(ingredient.router)
+    app.include_router(pantry_ingredient.router)
+    app.include_router(recipe.router)
