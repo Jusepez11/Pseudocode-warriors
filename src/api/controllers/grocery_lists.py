21c080efbee9c56788e778f5c6e4e641aa1bf48b
Sebastian Lopez-Gallardo <jusepez90@gmail.com>
2025-09-26 21:40:40 -0400

    Create grocery_lists.py
    I made it, but since we still need the model and schema, it needs changes

---
+++ src/api/controllers/grocery_lists.py
@@ -0,0 +1,89 @@
+from typing import Optional, List
+
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException, status, Response, Depends
+
+from src.api.models.ingredient import Ingredient as Model
+#from src.api.schemas.user import IngredientsCreate
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+'''
+TODO
+
+WE STILL NEED THE MODEL BUT THIS IS A TEMPLATE
+
+JUST CHANGE THE IMPORT INGREDIENT
+AND THE WHATS BEING IMPORTED FROM BELOW
+
+'''
+
+
+def create(db: Session, request):
+    if not request.id:
+        request.id = None
+
+    new_item = Model(
+        name=request.name,
+    )
+
+    try:
+        db.add(new_item)
+        db.commit()
+        db.refresh(new_item)
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+
+    return new_item
+
+
+def read_all(db: Session, skip: int = 0, limit: int = 100) -> List[type[Model]]:
+    try:
+        result = db.query(Model).offset(skip).limit(limit).all()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return result
+
+
+def read_one(db: Session, id):
+    try:
+        item = db.query(Model).filter(Model.id == id).first()
+        if not item:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+
+    return item
+
+def update(db: Session, id, request):
+    try:
+        item = db.query(Model).filter(Model.id == id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        update_data = request.dict(exclude_unset=True)
+        item.update(update_data, synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return item.first()
+
+
+def delete(db: Session, id):
+    try:
+        item = db.query(Model).filter(Model.id == id)
+        if not item.first():
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Id not found!")
+        item.delete(synchronize_session=False)
+        db.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=error)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
